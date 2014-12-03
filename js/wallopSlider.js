b175b204cb8258fee4956e029ed6d72a968afaae
Nishita Singh <nishita.nz@gmail.com>
2014-12-04 11:33:55 +1300

    Add smooth scrolling (jQeury)

--- js/wallopSlider.js
+++ js/wallopSlider.js
@@ -16,18 +16,26 @@ $(document).ready(function() {
 });
 
 
+// Smooth scrolling
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
 // Get current year for footer
 var currentYear = (new Date).getFullYear();
 $("#date").text( (new Date).getFullYear() );
-
-
-
-
-
-
-
-
-
 
 
 
